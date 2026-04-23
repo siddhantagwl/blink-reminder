ef61ef54571eb13d44a34c83dd9db29d58fdeef2
Siddhant Agarwal <siddhantagwl29@gmail.com>
2026-04-23 10:31:39 +0200

    Remove unused Forge+Vite template files

--- forge.config.js
+++ forge.config.js
@@ -43,27 +43,14 @@ module.exports = {
     {
       name: '@electron-forge/plugin-vite',
       config: {
-        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-        // If you are familiar with Vite configuration, it will look really familiar.
         build: [
           {
-            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
             entry: 'src/main.js',
             config: 'vite.main.config.mjs',
             target: 'main',
           },
-          {
-            entry: 'src/preload.js',
-            config: 'vite.preload.config.mjs',
-            target: 'preload',
-          },
         ],
-        renderer: [
-          {
-            name: 'main_window',
-            config: 'vite.renderer.config.mjs',
-          },
-        ],
+        renderer: [],
       },
     },
     // Fuses are used to enable/disable various Electron functionality
